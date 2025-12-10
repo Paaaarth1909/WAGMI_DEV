3acf3010f146dba6a7a40fc88fde136e20fbf5a9
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-10 23:19:18 +0530

    COMMIT

---
+++ FE_01/week_4/Product_Landing_Page/app.js
@@ -0,0 +1,6 @@
+let more = document.getElementById("more")
+let info = document.getElementById("info")
+
+more.onclick = () => {
+    info.style.display = info.style.display==="none" ? "block" : "none"
+}
